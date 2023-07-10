b1ecca8cd9971cf6a661782d7ef8b7046945f91f
DyhaCreator <andropet228@gmail.com>
2023-07-10 16:59:26 -0400

    display boids

--- Graph.h
+++ Graph.h
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <cmath>
 
 class Graph{
 public:
@@ -8,9 +9,11 @@ public:
     const int FrameRate = 100;
     sf::Event ev;
     sf::RenderWindow window = sf::RenderWindow(sf::VideoMode(1080,720), "Test Gen");
+
     Graph(){
         this->window.setFramerateLimit(FrameRate);
     }
+
     void update(){
         this->isOpen = this->window.isOpen();
         while(this->window.pollEvent(this->ev)){
@@ -21,9 +24,33 @@ public:
             }
         }
     }
-    void render(){
+
+    void render(int frame){
         this->window.clear(sf::Color(50,50,50));
+        
+        Draw_Boids(this->Width / 2, this->Height / 2, (float)frame / 1000.f, 500);
 
         this->window.display();
+    }
+
+    void Draw_Boids(int x, int y, float angle, int size){
+        const float BOTTOM_ANGLE = 0.6;
+
+        sf::ConvexShape convex;
+
+        convex.setPointCount(3);
+
+        convex.setPoint(0, sf::Vector2f(sin(angle) * size / 2 + x, y - cos(angle) * size / 2));
+        convex.setPoint(1, sf::Vector2f(sin(angle - M_PI + BOTTOM_ANGLE) * size / 2 + x, y - cos(angle - M_PI + BOTTOM_ANGLE) * size / 2));
+        convex.setPoint(2, sf::Vector2f(sin(angle - M_PI - BOTTOM_ANGLE) * size / 2 + x, y - cos(angle - M_PI - BOTTOM_ANGLE) * size / 2));
+
+        this->window.draw(convex);
+        /*
+        size /= 20;
+
+        sf::CircleShape circle(size);
+        circle.setPosition(x - size, y - size);
+        circle.setFillColor(sf::Color(250, 0, 0));
+        this->window.draw(circle);*/
     }
 };
--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 #include "Graph.h"
 
 int main(){
+    int frame = 0;
     Graph gr = Graph();
     while(gr.isOpen){
         gr.update();
-        gr.render();
+        gr.render(frame);
+        frame ++;
     }
     return 0;
 }
